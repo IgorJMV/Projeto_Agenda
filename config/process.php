562095aaea3e0c5c0cbdc9419de81c4332ef8ef6
IgorJMV <igorjmv2000@gmail.com>
2023-10-12 17:22:44 -0300

    Tela de registro de contato

--- config/process.php
+++ config/process.php
@@ -5,12 +5,29 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $contacts = [];
+    $id;
 
-    $query = "SELECT * FROM contacts";
+    if(!empty($_GET)){
+        $id = $_GET["id"];
+    }
+    if(!empty($id)){
+        //Retorna o dado de um contato
+        $query = "SELECT * FROM contacts WHERE id = :id";
+        $statement = $conn->prepare($query);
+        $statement->bindParam(":id", $id);
+        $statement->execute();
+        $contact = $statement->fetch();
+    }else{
+        //Retorna todos os contatos
+        $contacts = [];
+    
+        $query = "SELECT * FROM contacts";
+    
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        $contacts = $statement->fetchAll();
+    }
 
-    $statement = $conn->prepare($query);
-    $statement->execute();
-    $contacts = $statement->fetchAll();
+
 
 ?>
